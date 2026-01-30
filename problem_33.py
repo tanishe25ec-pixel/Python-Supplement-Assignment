7134b9aa60dc8d0bcb113363ad5fd76d7338b2c6
tanishe25ec-pixel <tanish.e_25ec@kgkite.ac.in>
2026-01-30 14:15:52 +0530

    Update problem_33.py

--- problem_33.py
+++ problem_33.py
@@ -2,10 +2,7 @@
 # Find and fix the error
 
 def is_all_digits(text):
-    for char in text:
-        if not char.isdigit():
-            return False
-    return True
+    return text.isdigit()
 
-test_str = "12345"
+test_str = "12345" 
 print(f"Is all digits: {is_all_digits(test_str)}")
