0fe03e97e610fadd8b9339a81bf89a1cf3c39bdc
tanishe25ec-pixel <tanish.e_25ec@kgkite.ac.in>
2026-01-30 14:51:58 +0530

    Update problem_52.py

--- problem_52.py
+++ problem_52.py
@@ -3,7 +3,7 @@
 
 def find_missing(numbers):
     n = len(numbers) + 1
-    expected_sum = n * (n + 1) / 2
+    expected_sum = n * (n + 1) // 2
     actual_sum = sum(numbers)
     return expected_sum - actual_sum
 
