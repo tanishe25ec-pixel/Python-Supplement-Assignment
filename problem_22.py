68bd3fa037beba8d9eb29f76fe4bdbe4f86c4d57
tanishe25ec-pixel <tanish.e_25ec@kgkite.ac.in>
2026-01-30 13:17:16 +0530

    Fix formatting issue in flattening nested list

--- problem_22.py
+++ problem_22.py
@@ -5,5 +5,5 @@ nested_list = [[1, 2], [3, 4], [5, 6]]
 flat_list = []
 for sublist in nested_list:
     for item in sublist:
-        flat_list.append(item)
+        flat_list.append(item) 
 print(f"Flattened list: {flat_list}")
