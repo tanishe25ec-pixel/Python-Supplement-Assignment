6e3c30586e30029b207249f0ffbe39cf8030f87d
tanishe25ec-pixel <tanish.e_25ec@kgkite.ac.in>
2026-01-30 15:02:36 +0530

    Update problem_61.py

--- problem_61.py
+++ problem_61.py
@@ -4,7 +4,7 @@
 def find_pairs(arr, target):
     pairs = []
     for i in range(len(arr)):
-        for j in range(i, len(arr)):
+        for j in range(i + 1, len(arr)):  # start from i+1
             if arr[i] + arr[j] == target:
                 pairs.append((arr[i], arr[j]))
     return pairs
