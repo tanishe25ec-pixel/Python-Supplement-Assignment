f9271ed559cd936b0db0fc62964fd155084a92b0
tanishe25ec-pixel <tanish.e_25ec@kgkite.ac.in>
2026-01-30 14:41:14 +0530

    Update problem_44.py

--- problem_44.py
+++ problem_44.py
@@ -1,7 +1,9 @@
-# Problem 44: Print Fibonacci sequence
-# Find and fix the error
-
 def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+
     fib = [0, 1]
     for i in range(2, n):
         fib.append(fib[i-1] + fib[i-2])
