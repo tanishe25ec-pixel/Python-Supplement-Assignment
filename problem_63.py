742aa890b588b346fbde3d36de96a00274347469
tanishe25ec-pixel <tanish.e_25ec@kgkite.ac.in>
2026-01-30 15:04:11 +0530

    Update problem_63.py

--- problem_63.py
+++ problem_63.py
@@ -3,7 +3,7 @@
 
 def find_longest_word(sentence):
     words = sentence.split()
-    longest = ""
+    longest = "" 
     for word in words:
         if len(word) > len(longest):
             longest = word
